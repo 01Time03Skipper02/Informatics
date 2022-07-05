268838b36c29a23b62b10b8279012bafde0d18de
01Time03Skipper02 <sasha.shvech2003@mail.ru>
2022-07-06 01:33:35 +0300

    Add some tasks
    #2

---
+++ №464.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int matrix[n][n];
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) {
+        cin >> matrix[i][j];
+    }
+    for (int i = 0; i < n; i++) for (int j = i+1; j < n; j++){
+        if (matrix[i][j] == 1){
+            cout << i+1 << " " << j+1 << endl;
+        }
+    }
+    return 0;
+}
